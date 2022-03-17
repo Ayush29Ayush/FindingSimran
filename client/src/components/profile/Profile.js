1b787234a4d71aecaa16a474520ae5436ea2ad70
Ayush Senapati <ayushsenapati123@gmail.com>
2022-03-17 23:14:34 +0530

    ProfileAbout added

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,7 +6,8 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfileById } from "../../actions/profile";
-import ProfileTop from "./ProfileTop"
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
 
 const Profile = ({
   getProfileById,
@@ -36,6 +37,7 @@ const Profile = ({
             )}
           <div className="profile-grid my-1">
             <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
           </div>
         </Fragment>
       )}

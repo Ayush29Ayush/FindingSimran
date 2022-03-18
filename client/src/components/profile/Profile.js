26f5e3d812cd73baf3eda39a4a5456d72adaf01e
Ayush Senapati <ayushsenapati123@gmail.com>
2022-03-18 14:09:23 +0530

    Added ProfileGithub

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -10,6 +10,7 @@ import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
 import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub";
 
 const Profile = ({
   getProfileById,
@@ -70,6 +71,9 @@ const Profile = ({
                 <h4>No education credentials</h4>
               )}
             </div>
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}

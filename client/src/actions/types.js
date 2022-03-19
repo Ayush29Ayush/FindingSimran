1ee39bb6d8d9c46fee25d375af14ff0a5972be6c
Ayush Senapati <ayushsenapati123@gmail.com>
2022-03-19 21:49:34 +0530

    new constant

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -19,3 +19,4 @@ export const GET_REPOS = "GET_REPOS";
 export const GET_POSTS = "GET_POSTS";
 export const POST_ERROR = "POST ERROR";
 export const UPDATE_LIKES = "UPDATE_LIKES";
+export const DELETE_POST = "DELETE_POST";

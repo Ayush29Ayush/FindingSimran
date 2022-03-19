2f29593771c759923dc286a1537c2e2b1464e779
Ayush Senapati <ayushsenapati123@gmail.com>
2022-03-19 19:21:44 +0530

    Add like

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -18,3 +18,20 @@ export const getPosts = () => async (dispatch) => {
     });
   }
 };
+
+//! Add like
+export const addLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};

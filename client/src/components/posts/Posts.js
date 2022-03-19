e1ad69e57d1c522837eae36159213ff80577b35c
Ayush Senapati <ayushsenapati123@gmail.com>
2022-03-19 15:27:24 +0530

    Show posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -5,13 +5,28 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getPosts } from "../../actions/post";
+import PostItem from "./PostItem";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return loading ? <Spinner /> : <Fragment>Testing...</Fragment>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome to the community
+      </p>
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {

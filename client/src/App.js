9c56a69e397ef7d1c0f777720a78a41378734e8f
Ayush Senapati <ayushsenapati123@gmail.com>
2022-03-20 21:31:50 +0530

    NotFound added to main App.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import Posts from "./components/posts/Posts";
 import Post from "./components/post/Post";
+import NotFound from "./components/layout/NotFound";
 
 //! Redux
 import { Provider } from "react-redux";
@@ -71,6 +72,7 @@ const App = () => {
               />
               <PrivateRoute exact path="/posts" component={Posts} />
               <PrivateRoute exact path="/posts/:id" component={Post} />
+              <Route component={NotFound} />
             </Switch>
           </section>
         </Fragment>

bb84b5c0e0798d4723f75dda15fccc0eff0d7c7c
Ayush Senapati <ayushsenapati123@gmail.com>
2022-03-20 11:44:06 +0530

    added PostForm

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -6,6 +6,7 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getPosts } from "../../actions/post";
 import PostItem from "./PostItem";
+import PostForm from "./PostForm";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
@@ -20,6 +21,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
       <p className="lead">
         <i className="fas fa-user" /> Welcome to the community
       </p>
+      <PostForm />
       <div className="posts">
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />

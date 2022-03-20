6302948c6aa9ef9e07d65f2654c7b28b46461873
Ayush Senapati <ayushsenapati123@gmail.com>
2022-03-20 21:14:40 +0530

    add and delete comment

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Moment from "react-moment";
+import { deleteComment } from "../../actions/post";
 
 const CommentItem = ({
   postId,
   comment: { _id, text, name, avatar, user, date },
   auth,
+  deleteComment,
 }) => (
   <div className="post bg-white p-1 my-1">
     <div>
@@ -23,6 +25,7 @@ const CommentItem = ({
       </p>
       {!auth.loading && user === auth.user._id && (
         <button
+          onClick={() => deleteComment(postId, _id)}
           type="button"
           className="btn btn-danger"
         >
@@ -37,10 +40,11 @@ CommentItem.propTypes = {
   postId: PropTypes.string.isRequired,
   comment: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  deleteComment: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { })(CommentItem);
+export default connect(mapStateToProps, { deleteComment })(CommentItem);
